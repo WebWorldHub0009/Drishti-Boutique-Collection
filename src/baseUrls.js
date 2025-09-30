c3c27b2acd262f804d2c91b69777e712ecd5f683
Mohd Shamshad <shmsadalam2@gmail.com>
2025-09-30 15:41:08 +0530

    initial commit

--- src/baseUrls.js
+++ src/baseUrls.js
@@ -1,2 +1,2 @@
- export const baseUrls = 'http://localhost:5000'
-// export const baseUrls = 'https://wanderlust-xhyk.onrender.com'
+//  export const baseUrls = 'http://localhost:5000'
+export const baseUrls = 'https://drishti-back.onrender.com'
