a451ea65cc00ff5a5c44f5b84c64e4d426982b77
Mohd Shamshad <shmsadalam2@gmail.com>
2025-10-18 11:12:39 +0530

    Blouse and sarees successfully added on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,12 +6,16 @@ import DrishtiGallery from '../Components/DrishtiGallery'
 import OwnerSection from '../Components/OwnerSection'
 import MostWantedProducts from '../Components/MostWantedProduct'
 import ShopByCategory from './ShopByCategory'
+import DesignerSarees from './DesignerSaree'
+import Blouse from './Blouse'
 
 const Home = () => {
   return (
     <>
     <HeroSection/>
     {/* <DrishtiServices/> */}
+    <DesignerSarees/>
+    <Blouse/>
     <ShopByCategory/>
     <WhyChooseUs/>
     <DrishtiGallery/>
